6a63817fcba27d05ffc0df95948ea257c7ec521b
VITORIA GABRIELLY SIMOES SILVA <89546415+Vitoriasimoes@users.noreply.github.com>
2024-06-26 10:47:48 -0300

    Add files via upload

---
+++ DesafioControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+        super("Parâmetros inválidos: o primeiro parâmetro deve ser menor que o segundo.");
+    }
+}
---
+++ DesafioControleFluxo/README.md
@@ -0,0 +1,2 @@
+# DesafioControleFluxo
+system of validation of selection process - sistema de validacao de processo seletivo - desafio DIO
